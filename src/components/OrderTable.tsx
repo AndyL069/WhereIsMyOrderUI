00280d68a087217502797e58d78d39f717b5b221
Andreas Lichtsinn <alichtsinn@campana-schott.com>
2020-11-12 18:28:28 +0100

    10 rows

--- src/components/OrderTable.tsx
+++ src/components/OrderTable.tsx
@@ -100,7 +100,7 @@ function OrderTable(props: {
                     selection={props.selectedOrders}
                     onSelectionChange={(e) => props.setSelectedOrders(e.value)}
                     className="p-datatable-responsive"
-                    dataKey="id" paginator rows={5} rowsPerPageOptions={[5, 10, 25]}
+                    dataKey="id" paginator rows={10} rowsPerPageOptions={[5, 10, 25]}
                     paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
                     currentPageReportTemplate="Showing {first} to {last} of {totalRecords} orders"
                     header={header}
